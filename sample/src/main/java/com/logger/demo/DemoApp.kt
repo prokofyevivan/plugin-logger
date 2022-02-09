3a639453241de01dc45067e1e39a086f7e6f2ec3
srtvprateek <srtv.prateek@gmail.com>
2022-02-09 22:06:55 +0530

    remove share pref plugin

--- sample/src/main/java/com/logger/demo/DemoApp.kt
+++ sample/src/main/java/com/logger/demo/DemoApp.kt
@@ -4,14 +4,12 @@ import android.app.Application
 import com.pluto.Pluto
 import com.pluto.plugins.logger.PlutoLoggerPlugin
 import com.pluto.plugins.logger.PlutoTimberTree
-import com.pluto.preferences.PlutoSharePreferencesPlugin
 import timber.log.Timber
 
 class DemoApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Pluto.Installer(this)
-            .addPlugin(PlutoSharePreferencesPlugin("sharedPref"))
             .addPlugin(PlutoLoggerPlugin("logger"))
             .install()
 
