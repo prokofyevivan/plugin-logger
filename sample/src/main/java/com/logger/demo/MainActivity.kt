d4088a9e9335971075dc0e0cb53c84d3da1687bd
srtvprateek <srtv.prateek@gmail.com>
2022-02-09 22:53:31 +0530

    ktlint format fixes

--- sample/src/main/java/com/logger/demo/MainActivity.kt
+++ sample/src/main/java/com/logger/demo/MainActivity.kt
@@ -5,8 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.logger.demo.databinding.ActivityMainBinding
 import com.pluto.plugins.logger.PlutoLog
 import com.pluto.plugins.logger.event
-import timber.log.Timber
 import java.lang.NullPointerException
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,10 +30,13 @@ class MainActivity : AppCompatActivity() {
             PlutoLog.i("demo", "info cta clicked")
         }
         binding.logEvent.setOnClickListener {
-            PlutoLog.event("demo", "event cta clicked", hashMapOf(
-                "attr_name" to "demo attr",
-                "timestamp" to System.currentTimeMillis()
-            ))
+            PlutoLog.event(
+                "demo", "event cta clicked",
+                hashMapOf(
+                    "attr_name" to "demo attr",
+                    "timestamp" to System.currentTimeMillis()
+                )
+            )
         }
 
         /**
@@ -46,16 +49,19 @@ class MainActivity : AppCompatActivity() {
             Timber.tag("error").e(NullPointerException("demo"), "timber_error cta clicked")
         }
         binding.timberVerbose.setOnClickListener {
-            Timber.tag("error").v( NullPointerException("demo"), "timber_verbose cta clicked")
+            Timber.tag("error").v(NullPointerException("demo"), "timber_verbose cta clicked")
         }
         binding.timberInfo.setOnClickListener {
             Timber.i("timber_info cta clicked")
         }
         binding.timberEvent.setOnClickListener {
-            Timber.tag("event").event("timber_event cta clicked", hashMapOf(
-                "attr_name" to "demo attr",
-                "timestamp" to System.currentTimeMillis()
-            ))
+            Timber.tag("event").event(
+                "timber_event cta clicked",
+                hashMapOf(
+                    "attr_name" to "demo attr",
+                    "timestamp" to System.currentTimeMillis()
+                )
+            )
         }
     }
 }
